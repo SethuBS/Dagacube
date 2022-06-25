bcdcc936956f66f1552d950ad22bc76921da822e
Sethu Budaza <sethu@terrafirma-solutions.com>
2022-06-25 17:22:36 +0200

    Transaction class

--- src/main/java/com/casino/dagacube/model/Transaction.java
+++ src/main/java/com/casino/dagacube/model/Transaction.java
@@ -1,4 +1,59 @@
 package com.casino.dagacube.model;
 
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "transaction")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Transaction {
+
+    @Id
+    @GeneratedValue
+    @Column(updatable = false, nullable = false)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "player_id")
+    private Player playerId;
+    @Column
+    private Double amount;
+    @Column
+    private TransactionType transactionType;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Player getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(Player playerId) {
+        this.playerId = playerId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(TransactionType transactionType) {
+        this.transactionType = transactionType;
+    }
 }
